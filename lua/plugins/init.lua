9194347e04d51f2d761f03129f42d8cc9c9fb164
BeauRussell <yourknightmares@gmail.com>
2024-12-08 15:20:45 -0600

    Set up colorscheme and telescope

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,6 +17,5 @@ return {
 	"nvim-lua/plenary.nvim",
 	{ "nvim-telescope/telescope.nvim", tag = '0.1.8', dependencies = { "nvim-lua/plenary.nvim" } },
 	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-	{ "ThePrimeagen/harpoon", dependencies = { "nvim-lua/plenary.nvim" } },
 	"tpope/vim-fugitive",
 }
