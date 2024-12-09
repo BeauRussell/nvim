cf81ef4ccaa3f19e1ef70c33c81998d05f567f91
BeauRussell <yourknightmares@gmail.com>
2024-12-08 20:49:54 -0600

    Testing

--- lua/plugins/zenmode.lua
+++ lua/plugins/zenmode.lua
@@ -14,18 +14,5 @@ return {
 			vim.wo.rnu = true
 			ColorMyPencils()
 		end)
-
-		vim.keymap.set("n", "<leader>zZ", function()
-			require("zen-mode").setup({
-				window = 80,
-				options = { }
-			})
-			require("zen-mode").toggle()
-			vim.wo.wrap = false
-			vim.wo.number = false
-			vim.wo.rnu = false
-			vim.opt.colorcolumn = "0"
-			ColorMyPencils()
-		end)
 	end
 }
