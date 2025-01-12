3122bbb4544ac7cd6ef6fcb9d26def14473b1114
BeauRussell <yourknightmares@gmail.com>
2025-01-11 20:25:12 -0600

    Add go snippet

--- lua/plugins/snippets.lua
+++ lua/plugins/snippets.lua
@@ -7,8 +7,25 @@ return {
 
 		config = function()
 			local ls = require("luasnip")
+			local s = ls.snippet
+			local t = ls.text_node
+
 			ls.filetype_extend("javascript", { "jsdoc" })
-			ls.filetype_extend("typescript", { "tsdoc" })	
+			ls.filetype_extend("typescript", { "tsdoc" })
+
+			ls.add_snippets("go", {
+				s("errno", {
+					t({
+						"if err != nil {",
+						"	log.Printf(\"Failed to do something: %v\", err)",
+						"	return err",
+						"}",
+					}),
+				}),
+			}, {
+				key = "go",
+			})
+
 			vim.keymap.set({"i"}, "<C-K>", function() ls.expand() end, {silent = true})
 			vim.keymap.set({"i", "s"}, "<leader>;", function() ls.jump(1) end, {silent = true})
 			vim.keymap.set({"i", "s"}, "<leader>,", function() ls.jump(-1) end, {silent = true})
