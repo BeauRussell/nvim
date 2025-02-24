51dfa0e705fdd44d82e93c4ed82a370ebc6cf148
Beau Russell <yourknightmares@gmail.com>
2025-02-23 19:33:14 -0600

    Remove assignment to local of telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -4,7 +4,7 @@ return {
 		"nvim-lua/plenary.nvim",
 	},
 	config = function()
-		local telescope = require("telescope").setup({})
+		require("telescope").setup({})
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 		vim.keymap.set('n', '<C-p>', builtin.git_files, {})
