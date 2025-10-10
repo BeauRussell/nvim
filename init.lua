737b8c019eb39b44a0f8d6dd21683ec8cd3f5ac1
Beau Russell <brussell@bigpanda.io>
2025-10-10 09:26:40 -0500

    GoTo Type Definition

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-vim.cmd("set relativenumber")
-vim.cmd("set number")
-vim.cmd("set shiftwidth=4")
-vim.cmd("set tabstop=4")
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.g.mapleader = " "
 require("config.lazy")
 
@@ -12,6 +12,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 	callback = function(e)
 		local opts = { buffer = e.buf }
 		vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+		vim.keymap.set("n", "gt", function() vim.lsp.buf.type_definition() end, opts)
 		vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
 		vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
 		vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
@@ -31,6 +32,7 @@ vim.api.nvim_create_autocmd('BufWritePre', {
 			vim.lsp.buf.format()
 			vim.lsp.buf.code_action { context = { only = { 'source.organizeImports' } }, apply = true }
 			vim.lsp.buf.code_action { context = { only = { 'source.fixAll' } }, apply = true }
+			vim.cmd('EslintFixAll')
 		end
 	end
 })
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -8,7 +8,6 @@ return {
 			sync_install = false,
 			auto_install = true,
 
-			indent = { enable = true },
 			highlight = {
 				enable = true,
 				additional_vim_regex_highlighting = { "markdown" }
