084f68d72652e5a11eeebfbb5ff8bf56a73a3bc0
Beau Russell <yourknightmares@gmail.com>
2026-01-08 07:02:56 -0600

    Add hidden files to telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -12,6 +12,14 @@ return {
 					treesitter = false,
 				},
 			},
+			pickers = {
+				find_files = {
+					no_ignore = true,
+					file_ignore_patterns = {
+						"node_modules"
+					}
+				}
+			}
 		})
 		local builtin = require('telescope.builtin')
 		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
